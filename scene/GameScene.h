1fc5be96fdae6528a50fbfa76f9756e06c661e8c
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-04-14 16:45:52 +0900

    更新

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -48,26 +48,31 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
+  private:
 	//テクスチャバンドル
 	uint32_t textureHandle_ = 0;
 	//スプライト
-	Sprite* sprite_ = nullptr;
+	//Sprite* sprite_ = nullptr;
 
+  private:
 	//3Dモデル
 	Model* model_ = nullptr;
 
+  private:
 	//ワールドトランスフォーム
 	WorldTransform worldTransform_;
 	//ビュープロジェクション
 	ViewProjection viewProjection_;
 
+  private:
 	//サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
+	//uint32_t soundDataHandle_ = 0;
 	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
+	//uint32_t voiceHandle_ = 0;
 
+  private:
 	//値を表示したい変数
-	int32_t value_ = 0;
+	//int32_t value_ = 0;
 
 	/// <summary>
 	/// ゲームシーン用
